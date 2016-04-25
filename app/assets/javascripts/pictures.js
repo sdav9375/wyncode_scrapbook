92f7a733f2f7be244af396efbdbd7a6f4798ffd0
christian abad <cabad20@gmail.com>
2016-04-25 19:00:40 -0400

    updated css/js styles

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,4 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+<script src="https://npmcdn.com/flickity@1.2/dist/flickity.pkgd.js"></script>
+<script src="/path/to/flickity.pkgd.min.js"></script>
 $('.fixed-action-btn').openFAB();
 $('.fixed-action-btn').closeFAB();
