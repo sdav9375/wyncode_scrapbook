f34f4226507a23ccd4efc2aabbbfa31a9d48d711
Susan Davies <sdav9375@gmail.com>
2016-04-25 19:44:21 -0400

    change to fields

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,6 @@
 class Picture < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
+
+  validates :cohort_id, numericality: { greater_than: 0, less_than: 10 }
+  validates :title, presence: true
 end
